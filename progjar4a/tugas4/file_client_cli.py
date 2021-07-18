01433fcaaac1103ac041263a46382292f91030bd
codestellations <erlindaargyt@gmail.com>
2021-07-18 16:16:37 +0700

    penyesuaian dengan simulator

--- progjar4a/tugas4/file_client_cli.py
+++ progjar4a/tugas4/file_client_cli.py
@@ -3,8 +3,6 @@ import json
 import base64
 import logging
 import datetime
-import threading
-import concurrent.futures
 from multiprocessing import Process, Pool
 
 server_address=('0.0.0.0',7777)
@@ -64,10 +62,10 @@ def remote_get(filename="", counter=0):
         nama = split[0].strip()
         ekstensi = split[1].strip()
 
-        namafile = "files" + "\\" + nama + "_" + str(counter) + "." + ekstensi
+        namafile = "files" + "/" + nama + "_" + str(counter) + "." + ekstensi
 
         isifile = base64.b64decode(hasil['data_file'])
-        fp = open(namafile,'wb+')
+        fp = open(namafile, 'wb+')
         fp.write(isifile)
         fp.close()
         return True
@@ -79,46 +77,10 @@ def remote_get_100(filename=""):
     texec = dict()
     status_task = dict()
 
-    task_pool = Pool(processes=10) #2 task yang dapat dikerjakan secara simultan, dapat diset sesuai jumlah core
-    task = concurrent.futures.ThreadPoolExecutor(max_workers=10)
+    # task yang dapat dikerjakan secara simultan, dapat diset sesuai jumlah core
+    task_pool = Pool(processes=100)
 
     catat_awal = datetime.datetime.now()
-
-    # multiprocess
-    # for k in range(0, 100):
-    #     print(f"mendownload {filename} {k+1}")
-    #
-    #     #bagian ini merupakan bagian yang mengistruksikan eksekusi fungsi download gambar secara multiprocess
-    #     texec[k] = Process(target=remote_get, args=(filename, k+1))
-    #     texec[k].start()
-    # #setelah menyelesaikan tugasnya, dikembalikan ke main process dengan join
-    # for k in range(0, 100):
-    #     texec[k].join()
-
-    # multithread async
-    # for k in range(1, 11):
-    #     print(f"mendownload {filename} {k}")
-    #
-    #     #bagian ini merupakan bagian yang mengistruksikan eksekusi fungsi download gambar secara multithread
-    #     texec[k] = task.submit(remote_get, filename, k)
-    #
-    #
-    # #setelah menyelesaikan tugasnya, dikembalikan ke main thread dengan memanggil result
-    # for k in range(0, 10):
-    #     status_task[k]=texec[k].result()
-    #
-
-    # multithread
-    # for k in range(0, 100):
-    #     print(f"mendownload {filename} {k+1}")
-    #
-    #     #bagian ini merupakan bagian yang mengistruksikan eksekusi fungsi download gambar secara multithread
-    #     texec[k] = threading.Thread(target=remote_get, args=(filename, k+1))
-    #     texec[k].start()
-    #
-    # #setelah menyelesaikan tugasnya, dikembalikan ke main thread dengan join
-    # for k in range(0, 100):
-    #     texec[k].join()
 
     # multiprocess async
     for k in range(0, 100):
@@ -134,13 +96,15 @@ def remote_get_100(filename=""):
     catat_akhir = datetime.datetime.now()
     selesai = catat_akhir - catat_awal
     print(f"Waktu TOTAL yang dibutuhkan {selesai} detik {catat_awal} s/d {catat_akhir}")
-    # print("status TASK")
-    # print(status_task)
 
     return
 
 if __name__=='__main__':
-    server_address=('127.0.0.1',7777)
-    remote_list()
-    # remote_get('donalbebek.jpg', 1)
+    # untuk pengujian di simulator
+    server_address=('192.168.122.76',7777)
     remote_get_100('pokijan.jpg')
+
+    # untuk pengujian di pycharm
+    # server_address=('127.0.0.1',7777)
+    # remote_list()
+    # remote_get('donalbebek.jpg')
